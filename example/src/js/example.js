74c5b678981df7e7db970204b9e4e08eecace022
Rosen Tsankov <cankovrosen@gmail.com>
2024-09-04 21:23:30 +0300

    remove example

--- example/src/js/example.js
+++
@@ -1,6 +0,0 @@
-import { CapacitorAndroidPlayer } from 'capacitor-android-player';
-
-window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    CapacitorAndroidPlayer.echo({ value: inputValue })
-}
